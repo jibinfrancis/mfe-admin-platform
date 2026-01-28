95ddc85bc3a40412a194b096b7a6bd05879cb4de
Jibin Francis <jibinfrancis996@gmail.com>
2026-01-28 18:18:51 +0530

    Header - Updated styles in header

--- apps/shell/src/components/header/Header.tsx
+++ apps/shell/src/components/header/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
         )?.label ?? "Dashboard";
 
     return (
-        <div className='h-14 p-2 font-semibold border-b-border border-b flex items-center'>
+        <div className='h-14 p-3 text-font-primary font-semibold border-b-border border-b flex items-center'>
             <div>{title}</div>
             <div className='flex flex-1 justify-end'>
                 <div className='w-11 h-11 bg-linear-120 rounded-full flex justify-center items-center text-white from-primary to-primary-soft'>A</div>
